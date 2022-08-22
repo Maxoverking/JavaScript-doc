09c76dcb046c54675f8a80b7a3871678d722d5df
Maxoverking <avemax7@gmail.com>
2022-08-22 22:08:32 +0200

    написал немного скрипта

---
+++ js/inputnumber.js
@@ -0,0 +1,28 @@
+const inputSrt = document.querySelector('.string[data-input]')
+const addBtn = document.querySelector('button[data-add]');
+const addReset = document.querySelector('button[data-clean]');
+const outPut = document.querySelector('.output span')
+
+let total = 0;
+
+addBtn.addEventListener('click', function () {
+    // console.log(addBtn);
+
+    const value = Number(inputSrt.value)
+
+
+    // console.log(value);
+    total += value;
+    outPut.textContent = total;
+    inputSrt.value = ' ';
+
+    console.log(total);
+})
+
+addReset.addEventListener('click', function () {
+    total = 0;
+    outPut.textContent = 0;
+    
+})
+
+
---
+++ js/try.js
@@ -0,0 +1,41 @@
+
+//ЗАДАЧА ВЫЧИСЛЕНИЕ СУММЫ ЧЕТНЫХ ЧИСЕЛ
+
+
+// const min = 7;
+// const max = 15;
+// let total = 0;
+
+// for (i = min; i <= max; i += 1) {
+
+//     if (i % 2 !== 0) {  
+//       console.log('Четное :', i); 
+//       continue;
+//     }
+  
+//   total +=  i;
+// }
+
+// console.log( 'Сумма :', total);
+
+
+
+// ЗЗЗЗЗЗадача для опраделения суммы заказа
+
+
+
+
+// let balance = 600;
+// const payment = 500;
+
+// console.log(`Ваша сумма ${balance} `);
+
+// if (balance >= payment) {
+
+//   console.log('OK');
+
+//   balance -= payment;
+//   console.log('Осталось :' , balance);
+// } else {
+//   console.log("Not money");
+// }
